f08912432e6fb575b170528d5e263cd756252123
Jie Zhang <jie@jetaport.com>
2020-07-14 21:39:23 -0700

    Notify user to reload app when switch language (#39)
    * Notify user to reload app when switch language
    
    * Let user choose to reload now or later
    
    * Skip confirm modal if switch back to original locale

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import './Settings.less';
 import Radio, { RadioChangeEvent } from 'antd/es/radio';
+import { Modal } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateLocale } from '../../redux/actions/localeActions';
 import LayoutConstants from '../../../constants/LayoutConstants.js';
+import { AppContext } from '../../context/AppContext';
 
 const Settings = () => {
-  const locale = useSelector((state: any) => state.locale.locale);
+  const locale = useContext(AppContext).locale;
   const size = useSelector((state: any) => state.layout[LayoutConstants.Settings]);
   const dispatch = useDispatch();
+  const [showConfirm, setShowConfirm] = useState(false);
   const { t } = useTranslation();
   const updateLocaleSettings = (event: RadioChangeEvent) => {
     dispatch(updateLocale({ locale: event.target.value }));
+    if (event.target.value !== locale) {
+      setShowConfirm(true);
+    }
   };
+  const reloadApp = () => {
+    window.location.reload();
+  }
+  const handleCancel = () => {
+    setShowConfirm(false);
+  }
   return (
     <div
       className="settings-container"
@@ -27,6 +39,14 @@ const Settings = () => {
           <Radio value={'zh-CN'}>{t('CHINESE')}</Radio>
         </Radio.Group>
       </div>
+      <Modal
+        title={t('RELOAD_APP')}
+        visible={showConfirm}
+        onOk={reloadApp}
+        onCancel={handleCancel}
+        okText={t('RELOAD_NOW')}
+        cancelText={t('RELOAD_LATER')}
+      ></Modal>
     </div>
   );
 };
